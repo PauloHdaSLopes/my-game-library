c95e03c5ce51e38eb70af9887dad9eac17c768e3
paulohdaslopes <paulohdas.lopes@gmail.com>
2021-07-19 17:20:57 -0300

    recently played page, map into the api result

--- src/pages/RecentlyPlayed/index.jsx
+++ src/pages/RecentlyPlayed/index.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import GameCard from "../../Components/GameCard";
 import { Subtitle } from "../../Components/UI";
 
+import { gamesFromApi } from "../../api/game"
+
 const RecentlyPlayed = () => {
   return (
-    <>
+    <div>
       <Subtitle>Recently played</Subtitle>
-      <GameCard />
-      <GameCard />
-      <GameCard />
-    </>
+      {
+        gamesFromApi.map( g => {
+          const genre = g.genre.reduce( (result, value) => result + " " + value)
+          const hasGameTime = g.gameTimes.length > 0
+          const gameTime = hasGameTime ?  g.gameTimes.reduce( (result, value) => `${result} ${value.name}: ${value.timeInHours} `, '') : "no data"
+          return <GameCard title={g.name} info={gameTime} genre={genre} />
+        })
+      }
+    </div>
   );
 };
 
