64ec486204adac40f810b30677dbf97d5aac4ac6
paulohdaslopes <paulohdas.lopes@gmail.com>
2021-07-19 17:19:03 -0300

    adjust in footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -3,22 +3,30 @@ import PlayArrow from "@material-ui/icons/PlayArrow";
 import ShowChart from "@material-ui/icons/ShowChart";
 import Settings from "@material-ui/icons/Settings";
 import { Link } from "react-router-dom";
-import { ContainerFooter } from "../UI";
+import { ContainerFooter, CircleBottomButton } from "../UI";
+
+import Add from "@material-ui/icons/Add";
 
 const Footer = () => {
-
   return (
-    <ContainerFooter>
-      <Link to="/">
-        <PlayArrow />
+    <>
+      <ContainerFooter>
+        <Link to="/">
+          <PlayArrow fontSize="large" />
+        </Link>
+        <Link to="/analytics">
+          <ShowChart fontSize="large" />
+        </Link>
+        <Link to="/settings">
+          <Settings fontSize="large" />
+        </Link>
+      </ContainerFooter>
+      <Link to="/add">
+        <CircleBottomButton>
+          <Add />
+        </CircleBottomButton>
       </Link>
-      <Link to="/analytics">
-        <ShowChart />
-      </Link>
-      <Link to="/settings">
-        <Settings />
-      </Link>
-    </ContainerFooter>
+    </>
   );
 };
 
